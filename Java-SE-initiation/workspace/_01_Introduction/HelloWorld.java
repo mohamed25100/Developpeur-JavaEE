95d9514413c1da51e9d0b7097c412be282d713be
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-26 11:18:03 +0200

    Create HelloWorld.java

---
+++ Java-SE-initiation/workspace/_01_Introduction/HelloWorld.java
@@ -0,0 +1,17 @@
+package _01_Introduction;
+
+public class HelloWorld {
+	
+	// https://thierry-leriche-dessirier.developpez.com/tutoriels/eclipse/raccourcis/
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		/*
+		 * Ceci est un commentaire
+		 * sur plusieurs
+		 * lignes...
+		 */
+	}
+
+}
