88d56fdcbb4946b127734f12d8944d341f07992d
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-26 11:18:42 +0200

    Create Print.java

---
+++ Java-SE-initiation/workspace/_02_Entrees_Sorties/Print.java
@@ -0,0 +1,22 @@
+package _02_Entrees_Sorties;
+
+public class Print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("La méthode 'printIn' permet d'écrire une ligne");
+		System.out.print("La méthode 'print' permet d'écrire");
+		System.out.print("une chaine de caractères");
+		String str = "printf"; // déclaration d'une variable de type String nommée 'str'
+		System.out.printf("La méthode %s permet d'ecrire des chanes de caractères formatées.", str);
+		int monEntier = 15;
+		System.out.printf("\nmonEntier = %d", monEntier);
+		
+		/* 
+		 * %s Permet de f
+		 * 
+		 * 
+		*/
+		
+	}
+}
