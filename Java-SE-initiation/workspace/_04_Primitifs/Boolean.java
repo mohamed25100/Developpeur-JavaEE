5c93c65948b953e7b6f35e7ff1ae383805d15881
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-26 13:00:35 +0200

    Create Boolean.java

---
+++ Java-SE-initiation/workspace/_04_Primitifs/Boolean.java
@@ -0,0 +1,15 @@
+package _04_Primitifs;
+
+public class Boolean {
+
+	public static void main(String[] args) {
+		/*
+		 * Le Type boolean peut définir 2 état :
+		 * - True
+		 * - False
+		 */
+		boolean isTrue = true;
+		boolean isFalse = false;
+	}
+
+}
