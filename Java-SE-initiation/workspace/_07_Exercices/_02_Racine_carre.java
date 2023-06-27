0964d9d8c5656c3dcb75a4a161f205fdd38de5ab
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-27 16:40:13 +0200

    Update _02_Racine_carre.java

--- Java-SE-initiation/workspace/_07_Exercices/_02_Racine_carre.java
+++ Java-SE-initiation/workspace/_07_Exercices/_02_Racine_carre.java
@@ -1,8 +1,25 @@
 package _07_Exercices;
 
+import java.util.Scanner;
+
 public class _02_Racine_carre {
 
 	//Ecrire un programme qui demande à l'utilisateur de rentrer un nombre entier et qui retourne sa racine carrée.
+	
+	public static double racineCarree(int nombre) {
+	return Math.sqrt(nombre);
+	}
+	
+	public static void main(String[] args) {
+		// La classe Scanner permet de récupérer des informations depuis la console
+		Scanner clavier = new Scanner(System.in);
 
+		System.out.print("Saisir un nombre entier : ");
+        int nombre = clavier.nextInt();
+
+        System.out.println("La racine carrée de " + nombre + " est : " + racineCarree(nombre));
+        
+        clavier.close();
+	}
 	
 }
