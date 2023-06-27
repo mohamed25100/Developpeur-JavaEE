19bfddf0408510329607a85674927578226b36f1
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-27 09:06:02 +0200

    Update and rename HelloWorld.java to HelloWorld.java

---
+++ Java-SE-initiation/workspace/_01_HelloWorld/HelloWorld.java
@@ -0,0 +1,18 @@
+package _01_HelloWorld;
+
+// Racourcis Eclipse : https://thierry-leriche-dessirier.developpez.com/tutoriels/eclipse/raccourcis/
+
+public class HelloWorld {
+	public static void main(String[] args) {
+
+		// Commentaire sur une ligne
+		System.out.println("Hello World");
+		
+		/*
+		 * Ceci est un commentaire 
+		 * sur plusieurs 
+		 * lignes...
+		 */
+	}
+}
+
