bfa1f94e7eca9233caa8f84b1f07aa47a937659f
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-27 17:01:05 +0200

    Update _05_Bissextile.java

--- Java-SE-initiation/workspace/_07_Exercices/_05_Bissextile.java
+++ Java-SE-initiation/workspace/_07_Exercices/_05_Bissextile.java
@@ -1,5 +1,7 @@
 package _07_Exercices;
 
+import java.util.Scanner;
+
 public class _05_Bissextile {
 	/*
 	 * Vérifier si l'année donnée par l'utilisateur est bissextile (366 jours) ou non.
@@ -8,4 +10,26 @@ public class _05_Bissextile {
 	 *  ou si elle est divisible par 400
 	 * « divisible » signifie que la division donne un nombre entier, sans reste (c'est à dire le reste égale à zéro) : 21 est divisible par 3. 22 non.
 	 */
+	public static boolean estAnneeBissextile(int annee) {
+	return (annee % 4 == 0 && annee % 100 != 0) || (annee % 400 == 0);
+	}
+	
+	public static void main(String[] args) {
+	
+	// La classe Scanner permet de récupérer des informations depuis la console
+	Scanner clavier = new Scanner(System.in);
+	
+	
+	System.out.print("Saisir une année : ");
+	int annee = clavier.nextInt();
+	
+	if (estAnneeBissextile(annee)) {
+	    System.out.println(annee + " est une année bissextile.");
+	} else {
+	    System.out.println(annee + " n'est pas une année bissextile.");
+	}
+	
+	clavier.close();
+	}
+
 }
