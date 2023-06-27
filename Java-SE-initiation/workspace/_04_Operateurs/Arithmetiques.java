2d27890c6a9605f3797e925dcdb2f6715844687e
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-27 10:02:11 +0200

    Create Arithmetiques.java

---
+++ Java-SE-initiation/workspace/_04_Operateurs/Arithmetiques.java
@@ -0,0 +1,20 @@
+package _04_Operateurs;
+
+public class Arithmetiques {
+
+	public static void main(String[] args) {
+		
+		int var1 = 10;
+		int var2;
+		
+		// il faut initaliser une variable avant de s'en servir
+		
+		var2 = 4;
+		
+		System.out.println("var1 + var2 = " + (var1 + var2));
+		System.out.println("var1 - var2 = " + (var1 - var2));
+		System.out.println("var1 * var2 = " + (var1 * var2));
+		System.out.println("var1 / var2 = " + (var1 / var2));
+		System.out.println("var1 % var2 = " + (var1 % var2)); // % : modulo : reste de la division ENTIERE
+	}
+}
