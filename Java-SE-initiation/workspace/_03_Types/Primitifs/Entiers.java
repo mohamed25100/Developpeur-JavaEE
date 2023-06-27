5617e84c284fe0b79fc208beda7d98e94530eee8
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-27 09:58:51 +0200

    Update and rename Java-SE-initiation/workspace/_04_Primitifs/Entiers.java to Java-SE-initiation/workspace/_03_Types/Primitifs/Entiers.java

--- Java-SE-initiation/workspace/_03_Types/Primitifs/Entiers.java
+++ Java-SE-initiation/workspace/_03_Types/Primitifs/Entiers.java
@@ -1,22 +1,31 @@
-package _04_Primitifs;
+package _03_Types.Primitifs;
 
 public class Entiers {
 
 	public static void main(String[] args) {
+
 		/*
+		 * Il existe 4 types entiers : byte, short, int, long.
 		 * 
+		 * Ces 4 types se différnecient par la taille (en nombre d'octets) des variables qu'iles permettent d'alouer.
 		 */
+		
 		byte b = 120;
 		short s = 32767;
 		int i = 2000000000;
 		long l = 2_000_000_000_000L;
 		
-		System.out.println(Byte.MIN_VALUE);
+		
 		System.out.printf("- %s (%d bits) from %d to %d\n", Byte.TYPE, Byte.SIZE, Byte.MIN_VALUE, Byte.MAX_VALUE);
 		System.out.printf("- %s (%d bits) from %d to %d\n", Short.TYPE, Short.SIZE, Short.MIN_VALUE, Short.MAX_VALUE);
 		System.out.printf("- %s (%d bits) from %d to %d\n", Integer.TYPE, Integer.SIZE, Integer.MIN_VALUE, Integer.MAX_VALUE);
 		System.out.printf("- %s (%d bits) from %d to %d\n", Long.TYPE, Long.SIZE, Long.MIN_VALUE, Long.MAX_VALUE);
-		short myBinaryShort = 0b11;// Notation binaire
+		
+		short myBinaryShort = 0b11; // Notation binaire
 		short myHexaShort = 0x61; // Notation hexadecimale
+		
+		System.out.println("myBinaryShort = " + myBinaryShort);
+		System.out.println("myHexaShort = " + myHexaShort);
 	}
 }
+
