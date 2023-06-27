1a3cb400450ef726d7e59fcaaae2f9fd192bfb93
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-27 16:53:46 +0200

    Update _04_Divisible.java

--- Java-SE-initiation/workspace/_07_Exercices/_04_Divisible.java
+++ Java-SE-initiation/workspace/_07_Exercices/_04_Divisible.java
@@ -1,5 +1,7 @@
 package _07_Exercices;
 
+import java.util.Scanner;
+
 public class _04_Divisible {
 
 	/*
@@ -8,4 +10,25 @@ public class _04_Divisible {
 	 * On peut écrire b = k*a + R, avec R = 0.
 	 */
 	
+	// La classe Scanner permet de récupérer des informations depuis la console
+    
+	public static boolean estDivisiblePar3Et13(int nombre) {
+        return nombre % 3 == 0 && nombre % 13 == 0;
+    }
+    
+	public static void main(String[] args) {
+		Scanner clavier = new Scanner(System.in);
+
+
+	    System.out.print("Saisir un nombre entier : ");
+	    int nombre = clavier.nextInt();
+
+	    if (estDivisiblePar3Et13(nombre)) {
+	        System.out.println(nombre + " est divisible par 3 et 13.");
+	    } else {
+	        System.out.println(nombre + " n'est pas divisible par 3 et 13.");
+	    }
+
+	    clavier.close();
+	}
 }
