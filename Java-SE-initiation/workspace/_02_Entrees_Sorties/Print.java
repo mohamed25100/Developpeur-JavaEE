db9959dbc0a97723c5c5853b493161a08ad64d29
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-27 09:06:50 +0200

    Update Print.java

--- Java-SE-initiation/workspace/_02_Entrees_Sorties/Print.java
+++ Java-SE-initiation/workspace/_02_Entrees_Sorties/Print.java
@@ -3,20 +3,33 @@ package _02_Entrees_Sorties;
 public class Print {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("La méthode 'printIn' permet d'écrire une ligne");
+		
+		System.out.println("La méthode 'println' permet d'écrire une ligne");
+		
 		System.out.print("La méthode 'print' permet d'écrire");
-		System.out.print("une chaine de caractères");
-		String str = "printf"; // déclaration d'une variable de type String nommée 'str'
-		System.out.printf("La méthode %s permet d'ecrire des chanes de caractères formatées.", str);
+		System.out.print(" une chaine de caractères");
+		System.out.print(" sans retour à la ligne.\n"); // \n : caractère de retour à la ligne
+		
+		String str = "printf"; // Déclaration d'une variable de type String nommée 'str'
+		
+		System.out.printf("La méthode %s permet d'écrire des chaines de caractères formatées.", str);
+		
 		int monEntier = 15;
+		
 		System.out.printf("\nmonEntier = %d", monEntier);
 		
-		/* 
-		 * %s Permet de f
-		 * 
-		 * 
-		*/
+		/*
+	     *  %s        Permet de formater une chaine de caractères
+	     *  %c        Permet de formater un caractère
+	     *  %d        Permet de formater un entier en base décimale.
+	     *  %x        Permet de formater un entier sous forme hexadécimale
+	     *  %e        Permet de formater un réel sous forme décimale en notation scientifique
+	     *  %f        Permet de formater un réel sous forme décimale
+	     *  %t        Utilisé pour formater les dates
+	     *  %n ou \n  (retour à la ligne)
+	     */
 		
+		System.out.printf("\nPI = %f", Math.PI);
+		System.out.printf("\nPI = %.3f", Math.PI);
 	}
 }
