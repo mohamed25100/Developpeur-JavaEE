94f8d0432c53444c1b7e2fa3415b2014736a5177
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-27 09:54:28 +0200

    Update and rename Boolean.java to Boolean.java

---
+++ Java-SE-initiation/workspace/_03_Types.Primitifs/Boolean.java
@@ -0,0 +1,21 @@
+package _03_Types.Primitifs;
+
+public class Boolean {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Le type boolen peut définir 2 état : 
+		 * - True
+		 * - False
+		 */
+
+		boolean isTrue = true;
+		boolean isFalse = false;
+		
+		System.out.println("isTrue = " + isTrue);
+		System.out.println("isFalse = " + isFalse);
+	}
+
+}
+
