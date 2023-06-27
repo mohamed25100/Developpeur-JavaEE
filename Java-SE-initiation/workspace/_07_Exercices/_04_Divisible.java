814268951b7b78112e31c2172d8f191ef009ca80
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-27 16:46:21 +0200

    Create _04_Divisible.java

---
+++ Java-SE-initiation/workspace/_07_Exercices/_04_Divisible.java
@@ -0,0 +1,11 @@
+package _07_Exercices;
+
+public class _04_Divisible {
+
+	/*
+	 * Vérifier si un nombre est divisible par 3 et 13 ou non.
+	 * Un nombre entier est divisible par un autre quand le résultat de la division euclidienne est un entier sans reste.
+	 * On peut écrire b = k*a + R, avec R = 0.
+	 */
+	
+}
