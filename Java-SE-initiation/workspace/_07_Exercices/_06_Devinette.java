090d8613f7fe88e93c5a743e0b0503a209109671
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-27 17:03:28 +0200

    Create _06_Devinette.java

---
+++ Java-SE-initiation/workspace/_07_Exercices/_06_Devinette.java
@@ -0,0 +1,19 @@
+package _07_Exercices;
+
+public class _06_Devinette {
+	/*
+	 * L'utilisateur doit deviner un nombre secret (généré automatiquement) entre 1 et 1000.
+	 * On va donc demander à l'utilisateur de trouver ce nombre secret.
+	 * Tant qu'il n'a pas trouvé ce nombre on lui demandera encore et encore, jusqu'à ce qu'il le trouve.
+	 * Si l'utilisateur choisit un nombre trop petit, l'application lui dira que le nombre qu’il a rentré est trop petit
+	 * Si l'utilisateur choisit un nombre trop grand, l'application lui dira que le nombre qu’il a rentré est trop grand
+	 * Si l'utilisateur trouve le nombre recherché, l'application lui indiquera le nombre d'essais dont il a eu besoin
+	 * 
+	 * Exemple :
+	 * Entrée : Entrer un nombre entre 1 et 1000 (1 - 1000) : 500
+	 * Sorties possibles :
+	 * Votre nombre est trop grand !
+	 * Votre nombre est trop petit !
+	 * Trouvé en 8 essais ! Bien Joué !
+	 */
+}
