b75344c95f6dff453590786732c2139c6d0851e8
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-27 17:04:35 +0200

    Create _09_Factorielle.java

---
+++ Java-SE-initiation/workspace/_07_Exercices/_09_Factorielle.java
@@ -0,0 +1,10 @@
+package _07_Exercices;
+
+public class _09_Factorielle {
+	/*
+	 * Demander à l'utilisateur de choisir un nombre entier et retourner sa
+	 * factorielle. Utiliser la récursivité.
+	 * n! = n*(n-1)! si n!=1
+	 * 1! = 1;
+	 */
+}
