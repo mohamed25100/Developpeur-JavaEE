fe083b330316aa204bcb5e1f38d18090208407b0
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-27 10:11:17 +0200

    Create Comparaison.java

---
+++ Java-SE-initiation/workspace/_04_Operateurs/Comparaison.java
@@ -0,0 +1,17 @@
+package _04_Operateurs;
+
+public class Comparaison {
+
+	public static void main(String[] args) {
+
+		int a = 1, b = 2;
+
+		System.out.println("a == b : " + (a == b));
+		System.out.println("a != b : " + (a != b));
+		System.out.println("a > b : " + (a > b));
+		System.out.println("a < b : " + (a < b));
+		System.out.println("a <= b : " + (a <= b));
+		System.out.println("a >= b : " + (a >= b));
+	}
+}
+
