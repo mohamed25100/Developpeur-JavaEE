1555f5d9a8eb027f9326a4d37a887d3de8a5f0be
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-27 16:54:24 +0200

    Create _05_Bissextile.java

---
+++ Java-SE-initiation/workspace/_07_Exercices/_05_Bissextile.java
@@ -0,0 +1,11 @@
+package _07_Exercices;
+
+public class _05_Bissextile {
+	/*
+	 * Vérifier si l'année donnée par l'utilisateur est bissextile (366 jours) ou non.
+	 * Une année est considérée comme une année bissextile si :
+	 * elle est divisible par 4 et non divisible par 100 ;
+	 *  ou si elle est divisible par 400
+	 * « divisible » signifie que la division donne un nombre entier, sans reste (c'est à dire le reste égale à zéro) : 21 est divisible par 3. 22 non.
+	 */
+}
