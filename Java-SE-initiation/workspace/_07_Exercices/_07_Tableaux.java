96d5800a055d49a71361c13f4cea41bf39dfe6e5
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-27 17:04:02 +0200

    Create _07_Tableaux.java

---
+++ Java-SE-initiation/workspace/_07_Exercices/_07_Tableaux.java
@@ -0,0 +1,25 @@
+package _07_Exercices;
+
+public class _07_Tableaux {
+
+	/* Insertion dans un tableau trié
+	 * 
+	 * Il faut insérer la nouvelle donnée directement à la bonne place. C’est le mécanisme de base du tri par insertion.
+	 * L’insertion d’une donnée à une place précise libère la case qui doit recevoir la donnée,
+	 * en décalant d’une case toute la partie droite du tableau à partir de cette position, puis copie la donnée dans la case libérée.
+	 * Pour éviter toute perte de données, le décalage part de la fin pour remonter ver la position d’insertion.
+	 * Le décalage débute à la dernière case qui contient une donnée, se poursuit sur l’avant-dernière case, et ainsi de suite.
+	 * 
+	 * Créer et initialiser un tableau, puis insérer un élément à la position spécifiée dans ce tableau (de 0 à N-1).
+	 * Pour insérer un nouvel élément dans le tableau, déplacez les éléments de la position d'insertion donnée vers une position vers la droite.
+	 * 
+	 * Exemple
+	 * Données d'entrée : Saisir le nombre de notes : 4
+	 * Note 1 : 8.5
+	 * Note 2 : 9.5
+	 * Note 3 : 12.5
+	 * Note 4 : 18.0
+	 * Saisir la note 5 : 11.0
+	 * Données de sortie : [8.5, 9.5, 11.0, 12.5, 18.0]
+	 */
+}
