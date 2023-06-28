64cc667a768e65877642de88171c25451bbd45cc
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-28 11:24:27 +0200

    Update _07_Tableaux.java

--- Java-SE-initiation/workspace/_07_Exercices/_07_Tableaux.java
+++ Java-SE-initiation/workspace/_07_Exercices/_07_Tableaux.java
@@ -1,7 +1,10 @@
 package _07_Exercices;
 
-public class _07_Tableaux {
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
+public class _07_Tableaux {
 	/* Insertion dans un tableau trié
 	 * 
 	 * Il faut insérer la nouvelle donnée directement à la bonne place. C’est le mécanisme de base du tri par insertion.
@@ -22,4 +25,46 @@ public class _07_Tableaux {
 	 * Saisir la note 5 : 11.0
 	 * Données de sortie : [8.5, 9.5, 11.0, 12.5, 18.0]
 	 */
+
+	public static void main(String[] args) {
+
+		// La classe Scanner permet de récupérer des informations depuis la console
+		Scanner clavier = new Scanner(System.in);
+		
+		Random random = new Random();
+		
+		System.out.print("Saisir le nombre de notes : ");
+		int tailleTableau = clavier.nextInt();
+		
+		double[] tableau = new double[tailleTableau + 1];
+		
+		for (int i = 0; i < tailleTableau; i++) {
+		    double note = Math.round(random.nextDouble() * 200) / 10.0; // Génère une note aléatoire avec deux décimales
+		    tableau[i] = note;
+		    System.out.printf("Note %d : %.2f%n", i + 1, note);
+		}
+
+	        System.out.print("Saisir la note à insérer : ");
+	        double nouvelleNote = Math.round(clavier.nextDouble() * 100) / 100.0; // Limite la nouvelle note à deux décimales
+	
+	        System.out.print("Choisir l'emplacement d'insertion (de 0 à " + tailleTableau + ") : ");
+	        int positionInsertion = clavier.nextInt();
+	
+	        deplacerElements(tableau, positionInsertion, tailleTableau);
+	        tableau[positionInsertion] = nouvelleNote;
+	
+	        System.out.print("Données de sortie : [");
+	        for (int i = 0; i < tailleTableau; i++) {
+	            System.out.printf("%.2f, ", tableau[i]);
+	        }
+	        System.out.printf("%.2f]%n", tableau[tailleTableau]);
+	
+	        clavier.close();
+	}
+
+	public static void deplacerElements(double[] tableau, int positionInsertion, int tailleTableau) {
+		for (int i = tailleTableau - 1; i >= positionInsertion; i--) {
+		    tableau[i + 1] = tableau[i];
+		}
+	}
 }
