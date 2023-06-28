e02b91e5872ffb0829bdebb7614085ea85ab51cb
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-28 10:12:21 +0200

    Update _09_Factorielle.java

--- Java-SE-initiation/workspace/_07_Exercices/_09_Factorielle.java
+++ Java-SE-initiation/workspace/_07_Exercices/_09_Factorielle.java
@@ -1,5 +1,7 @@
 package _07_Exercices;
 
+import java.util.Scanner;
+
 public class _09_Factorielle {
 	/*
 	 * Demander à l'utilisateur de choisir un nombre entier et retourner sa
@@ -7,4 +9,29 @@ public class _09_Factorielle {
 	 * n! = n*(n-1)! si n!=1
 	 * 1! = 1;
 	 */
+	
+	public static long calculerFactorielle(int nombre) {
+		if (nombre == 0 || nombre == 1) {
+		    return 1;
+		} else {
+		    return nombre * calculerFactorielle(nombre - 1);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		
+	// La classe Scanner permet de récupérer des informations depuis la console
+	Scanner clavier = new Scanner(System.in);
+
+        System.out.print("Entrez un nombre entier : ");
+        int nombre = clavier.nextInt();
+        
+        long factorielle = calculerFactorielle(nombre);
+
+        System.out.println(nombre + "! = " + factorielle);
+
+        clavier.close();
+	}
+
 }
