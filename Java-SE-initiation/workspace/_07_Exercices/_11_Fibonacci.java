760fe18cb248be73655adb6805e6774938cd092a
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-28 10:25:19 +0200

    Create _11_Fibonacci.java

---
+++ Java-SE-initiation/workspace/_07_Exercices/_11_Fibonacci.java
@@ -0,0 +1,24 @@
+package _07_Exercices;
+
+public class _11_Fibonacci {
+	/*
+	 * 
+	 * En mathématiques, la suite de Fibonacci est une suite d'entiers dans laquelle chaque terme est la somme des deux termes qui le précèdent.
+	 * Elle commence par les termes 0 et 1 si on part de l'indice 0, ou par 1 et 1 si on part de l'indice 1.
+	 * Dans les exercices suivants, nous nous concentrerons sur des tableaux à une dimension. Nous vous montrerons comment implémenter et utiliser des tableaux.
+	 * 
+	 * Exemple : 1 1 2 3 5 8 13 21 34….
+	 * 
+	 * Notée (Fn), elle est donc définie par :
+	 * 
+	 * F0 = 0,
+	 * F1 = 1 et
+	 * Fn = Fn-1 + Fn-2 pour n >= 2
+	 * 
+	 * On peut définir cette suite par récurrence.
+	 * 
+	 * On initialise les formules avec deux conditions initiales : F0 = 0, F1 = 1
+	 * Formule de récurrence : Fn = Fn-1 + Fn-2 pour n >= 2
+	 */
+}
+
