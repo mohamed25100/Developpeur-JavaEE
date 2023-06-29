1fad925233f038ea87d2aacfbf24bfea6a39c84d
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-29 12:03:55 +0200

    Create Carre2.java

---
+++ Java-SE-initiation/workspace/_02_POO/src/_05_Abstraction/Carre2.java
@@ -0,0 +1,9 @@
+package _05_Abstraction;
+
+public class Carre2 extends Rectangle{
+
+	public Carre2(int cote)
+	{
+		super(cote, cote);// Appel au constructeur à 2 paramètres de la classe Rectangle
+	}
+}
