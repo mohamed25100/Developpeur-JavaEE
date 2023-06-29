766ed99358cef567fe762e81a5fc2c31d0dea50b
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-29 12:02:00 +0200

    Create App.java

---
+++ Java-SE-initiation/workspace/_02_POO/src/_04_Heritage/App.java
@@ -0,0 +1,11 @@
+package _04_Heritage;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Client client = new Client("duck", "riri", 12, "+33 6 65 45 85 75");
+		
+		System.out.println(client);
+	}
+}
