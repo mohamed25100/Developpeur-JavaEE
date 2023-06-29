eb20ba42e77f14ed13bd36c8836623c1adcebe7d
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-29 12:02:28 +0200

    Create Client.java

---
+++ Java-SE-initiation/workspace/_02_POO/src/_04_Heritage/Client.java
@@ -0,0 +1,37 @@
+package _04_Heritage;
+
+public class Client extends User{
+
+	private String phone;
+	
+	private boolean isSubscriber;
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public boolean isSubscriber() {
+		return isSubscriber;
+	}
+
+	public void setSubscriber(boolean isSubscriber) {
+		this.isSubscriber = isSubscriber;
+	}
+
+	public Client(String nom, String prenom, int age, String phone) {
+		super(nom, prenom, age); // appel du constructeur à 3 paramètres de la classe mère (User)
+		this.phone = phone;
+		
+		int test = super.age; // super.age est accessible car marqué comme protected dans la classe 'User'
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "Je suis client. Mon numéro est " + this.getPhone();
+	}
+	
+}
