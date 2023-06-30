5e86e7391da66b30ca2c7d2a8c4e8cf9cd63f180
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2023-06-30 09:30:30 +0200

    Create Desktop.java

---
+++ Java-SE-initiation/workspace/_02_POO/src/_11_Sealed/Desktop.java
@@ -0,0 +1,10 @@
+package _11_Sealed;
+
+// Une classe héritant d'une classe 'sealed' doit au choix : 
+// - final : la classe ne pourra plus être héritée
+// - sealed : la classe pourra être héritée par une liste de classes autorisées
+// - non-sealed : la classe pourra être héritée sans restriction
+
+public non-sealed class Desktop extends Computer{
+
+}
